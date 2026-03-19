6ef1f91498b41aac3ff9a7afc3be45a4a02dc7ed
tngomarie <tn.ngo.marie8@gmail.com>
2026-03-19 17:28:05 +0100

    Revert "ajout de la bd"
    This reverts commit cbad3faec010ce57675dc8dce4f9def44b4832e6.

--- SAE4A_2026_JPO-TRACKER-Backen/app.py
+++ SAE4A_2026_JPO-TRACKER-Backen/app.py
@@ -1,12 +1,5 @@
-from flask import Flask, render_template, url_for, request, redirect
-from pymongo import MongoClient
-
+from flask import Flask
 app = Flask(__name__)
-
-client = MongoClient('localhost', 27017)
-
-db = client.flask_database
-@app.route('/', methods=['GET', 'POST'])
 
 @app.route('/')
 def hello_world():  # put application's code here
