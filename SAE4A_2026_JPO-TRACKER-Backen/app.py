cbad3faec010ce57675dc8dce4f9def44b4832e6
tngomarie <tn.ngo.marie8@gmail.com>
2026-03-19 14:45:53 +0100

    ajout de la bd

--- SAE4A_2026_JPO-TRACKER-Backen/app.py
+++ SAE4A_2026_JPO-TRACKER-Backen/app.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
+from pymongo import MongoClient
+
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+
+db = client.flask_database
+@app.route('/', methods=['GET', 'POST'])
 
 @app.route('/')
 def hello_world():  # put application's code here
